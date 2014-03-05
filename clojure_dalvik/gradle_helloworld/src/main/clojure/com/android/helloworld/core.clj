ce51801797ba6258993dbdbbb724c08032e2e930
Nicholas Kariniemi <nicholas.kariniemi@gmail.com>
2014-03-05 15:43:11 +0200

    Remove unused classes

--- clojure_dalvik/gradle_helloworld/src/main/clojure/com/android/helloworld/core.clj
+++ clojure_dalvik/gradle_helloworld/src/main/clojure/com/android/helloworld/core.clj
@@ -3,11 +3,8 @@
    :extends android.app.Activity
    :exposes-methods {onCreate superOnCreate})
   (:import [android.app Activity]
-           [android.os Bundle]
-           [android.util Log]
-           [android.os Debug]))
+           [android.os Bundle]))
 
 (defn -onCreate [this #^android.os.Bundle bundle ]
   (.superOnCreate this bundle)
-  (.setContentView this com.android.helloworld.R$layout/main)
-  (Log/v "com.android.helloworld.core" "Clojure!"))
+  (.setContentView this com.android.helloworld.R$layout/main))
